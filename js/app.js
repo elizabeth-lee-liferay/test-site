7775de15642c510202a7655d19ac3dc1ae9a4ce2
Your-Full-Name <your-email-address>
2020-02-25 16:36:12 -0800

    modified method of implementing hide button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-function hideTitle()
+function hideTitle()//function is called whenever the button is clicked
 {
-    document.getElementById("title").style.display='none';
+    // document.getElementById("main_title").style.visibility = 'hidden'; //should style.visibility be in style.css
+    // document.getElementById("main_title").removeAttribute("id");// removes the id attribute in style.css for element "main_title"
+    console.log(document.getElementsByClassName("title")); //documents things in the developer console
 }

999c950aa84b91f19e703326fa83bb61601b5627
Your-Full-Name <your-email-address>
2020-02-26 10:19:09 -0800

    Created object called person and called a function that displays the  object properties/methods

--- js/app.js
+++ js/app.js
@@ -3,13 +3,21 @@ function displayDate()
     document.getElementById("dateDisplay").innerHTML = Date();
 }
 
+function displayPersonalInfo()
+{
+    document.getElementById("personalInfoDisplay").innerHTML = person.fullName()+", "+person.age+", "+person.gender;
+}
 
 var person =
 {
     firstName: "Elizabeth", 
     lastName: "Lee", 
     age: 24, 
-    gender: "female"
+    gender: "female",
+    fullName: function()
+    {
+        return this.firstName + " " + this.lastName;
+    }
 };
 
-alert(person.age);
+
